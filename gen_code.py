4e943db450a45ff717d0412e3453366ea2c55106
cainex <dmgrabowski@gmail.com>
2019-08-29 09:12:37 -0400

    uop generation changes

--- gen_code.py
+++ gen_code.py
@@ -1,5 +1,6 @@
 from absl import app
 from absl import flags
+import json
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("filename", None, "Input definition file")
@@ -13,18 +14,12 @@ def main(argv):
     uops = {}
 
     with open(FLAGS.filename) as fp:
-        line = fp.readline()
-        while line:
-            split_line = line.split()
-            uops[split_line[0]] = {'uop' : split_line[1], 'addr_mode' : split_line[2], 'cycles' : split_line[3]}
-            line = fp.readline()
+        uops = json.load(fp)
 
-    print("{")
-    print("\t\"uops\": [")
-    for k,v in uops.items():
-        print("\t\t{{\"uop\":\"{}\", \"opcode\":\"{}\", \"addr_mode\":\"{}\", \"cycles\":\"{}\"}},".format(v["uop"], k, v['addr_mode'], v['cycles']))
-    print("\t]")
-    print("}")
+    print("DECODER INFORMATION")
+    for uop in uops['uop_decode']:
+        print(uop)
+    print("END DECODER")
 
 if __name__ == '__main__':
     app.run(main)
