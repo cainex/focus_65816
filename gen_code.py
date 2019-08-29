31a9fea5e2ca14d57bf14d5f26450e4b90b0f53d
cainex <dmgrabowski@gmail.com>
2019-08-29 09:43:06 -0400

    uop code gen changes

--- gen_code.py
+++ gen_code.py
@@ -1,5 +1,6 @@
 from absl import app
 from absl import flags
+from jinja2 import Template
 import json
 
 FLAGS = flags.FLAGS
@@ -8,6 +9,21 @@ flags.DEFINE_string("filename", None, "Input definition file")
 # Required flag.
 flags.mark_flag_as_required("filename")
 
+uop_template_string = """
+    if (uop == {{ opcode }} ) { // {{ uop }} {{ addr_mode }}
+        std::cout << "{{ uop }}  {{ addr_mode }}" << std::endl;
+        std::shared_ptr<AddressingMode> addressingMode(new {{addr_mode}}Mode(m_mem, m_reg));
+        std::shared_ptr<OpCode> opCode(new {{ uop }}(addressingMode, m_mem, m_reg));
+        m_reg->pc(m_reg->pc() + 1);
+        return opCode;
+    }
+"""
+
+uop_template = Template(uop_template_string)
+
+def render_uop(uop):
+    return uop_template.render(opcode=uop['opcode'], uop=uop['uop'], addr_mode=uop['addr_mode'])
+    
 def main(argv):
     del argv  # Unused.
 
@@ -16,10 +32,8 @@ def main(argv):
     with open(FLAGS.filename) as fp:
         uops = json.load(fp)
 
-    print("DECODER INFORMATION")
     for uop in uops['uop_decode']:
-        print(uop)
-    print("END DECODER")
+        print(render_uop(uop))
 
 if __name__ == '__main__':
     app.run(main)
